ff1a4131c5a5ef22d81a1b33a1f265e42a716a29
raquelghdez <al23760804@ite.edu.mx>
2025-03-15 12:43:01 -0700

    ACTUAL 15 MARZO 2025

---
+++ lib/suma.py
@@ -0,0 +1,12 @@
+
+def suma(a, b):
+    return a+b
+
+def main():
+    a=3
+    b=3
+    c=suma(a,b)
+    print(c)
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,11 @@
+
+from lib.suma import *
+
+def main():
+    a=2
+    b=2
+    c=suma(a,b)
+    print(c)
+
+if __name__ == '__main__':
+    main()
